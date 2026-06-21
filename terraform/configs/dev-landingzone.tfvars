8579b60099053767e1d66e84878441e5647ae9b6
martinpdavidseo <martinpdavidseo@gmail.com>
2026-06-21 18:28:35 +0530

    cheking vnet

---
+++ terraform/configs/dev-landingzone.tfvars
@@ -0,0 +1,4 @@
+environment  = "dev"
+location     = "centralindia"
+project_name = "resume"
+subscription_id    = "73fe9082-9618-44f2-8589-be0720ce898d"
---
+++ terraform/landingzone/variables.tf
@@ -0,0 +1,16 @@
+variable "environment" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
