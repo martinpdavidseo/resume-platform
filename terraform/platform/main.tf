6acc2d6382d5d1c5d60f3628d2ce55ab2a10cb73
martinpdavidseo <martinpdavidseo@gmail.com>
2026-07-06 22:17:47 +0530

    updated

--- terraform/platform/main.tf
+++ terraform/platform/main.tf
@@ -30,9 +30,7 @@ resource "azurerm_key_vault" "platform" {
 resource "azurerm_role_assignment" "keyvault_admin" {
 
   scope = azurerm_key_vault.platform.id
-
   role_definition_name = "Key Vault Administrator"
-
-  principal_id = var.github_sp_object_id
+  principal_id = data.azurerm_client_config.current.object_id
 
 }
