9b09590ef3a8bcf89f23e15308c1d6ae417e550a
martinpdavidseo <martinpdavidseo@gmail.com>
2026-07-06 17:50:23 +0530

    keyvault

--- terraform/platform/outputs.tf
+++ terraform/platform/outputs.tf
@@ -5,3 +5,15 @@
 # output "acr_name" {
 #   value = azurerm_container_registry.acr.name
 # }
+
+output "keyvault_name" {
+  value = azurerm_key_vault.platform.name
+}
+
+output "keyvault_id" {
+  value = azurerm_key_vault.platform.id
+}
+
+output "keyvault_uri" {
+  value = azurerm_key_vault.platform.vault_uri
+}
